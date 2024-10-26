93d14308110599f2a71d5a31439c2344a17e7d4c
Boris Grigorov <borisgrigorov@vvveb.cz>
2024-10-27 00:09:29 +0200

    Added SEO tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import About from "../components/About/About.svelte";
     import Experiences from "../components/Experiences/Experiences.svelte";
     import Contact from "../components/Contact/Contact.svelte";
+    import Seo from "../components/Seo.svelte";
+    import {page}from "$app/stores";
 </script>
 
 <Landing />
@@ -11,3 +13,10 @@
     <Experiences />
     <Contact />
 </div>
+
+<Seo
+    title="Boris Grigorov | Software Engineer"
+    description="Creative developer focused on building impactful web and mobile applications with seamless user experiences. Explore my portfolio to see projects that blend innovation with real-world usability"
+    image="{$page.url.host}/images/og.svg"
+    sitename="Boris Grigorov"
+/>
