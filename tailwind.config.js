09708fa3c87e56e7dab6a257068807c5a922b0d5
Boris Grigorov <borisgrigorov@vvveb.cz>
2024-10-26 21:46:50 +0200

    Basic settings, added font and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
     content: ["./src/**/*.{svelte,js,ts,html}"],
     theme: {
         extend: {},
+        colors: {
+            primary: "#FFFFFF",
+            accent: "#DC2626",
+            background: "#04081A",
+            secondary: "#afafaf",
+        },
     },
     plugins: [],
 };
